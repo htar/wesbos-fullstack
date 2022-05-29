30642ac3d8b1e455814f687ae704a3d24f6dbcf9
Taras Khmelovskyi <khmelovski@gmail.com>
2022-05-29 14:15:53 +0300

    add more data to DB by insert SeedData

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -8,6 +8,7 @@ import 'dotenv/config';
 import { User } from './schemas/User';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
@@ -39,12 +40,18 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
+      async onConnect(keystone) {
+        console.log('Connected to MongoDB', keystone);
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
       // TODO: Add data seeding here
     },
     lists: createSchema({
       User,
       Product,
-      ProductImage
+      ProductImage,
       // Schema items go in here
     }),
     ui: {
@@ -52,7 +59,7 @@ export default withAuth(
       isAccessAllowed: ({ session }) => {
         console.log('session', session);
 
-        return !!session?.data; 
+        return !!session?.data;
       },
     },
     session: withItemData(statelessSessions(sessionConfig), {
